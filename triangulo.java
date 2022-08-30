f50d9a73394172cf5f85eadc6de6d64c1a427f9f
javierhuebra <javieremanuelhuebra@gmail.com>
2022-08-30 20:40:06 -0300

    first upload

---
+++ triangulo.java
@@ -0,0 +1,16 @@
+
+public class triangulo {
+
+	public static void main(String[] args) {
+		
+		int i, j, row=10;
+		
+		for(i=0;i<row;i++) {
+			for(j=0;j<i;j++) {
+				System.out.printf("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
